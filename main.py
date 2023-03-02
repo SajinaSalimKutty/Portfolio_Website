9c098214a18887a99af42efb9e23e8af2103de51
Sajina Salim Kutty <sajinassk03@gmail.com>
2023-03-02 16:30:44 +0530

    Details Updated

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
-
+st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("Images/IMG.png")
+    st.image("Images/IMG.png",width=300)
 
 with col2:
     st.title("Sajina Salim Kutty")
