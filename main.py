ebb8ca99050d58e185257d8d1c0eb1122db81265
Sajina Salim Kutty <sajinassk03@gmail.com>
2023-03-14 13:01:04 +0530

    Render Titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -17,4 +18,15 @@ content2 = """
 Below are some of the apps built in python.
 """
 st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index,row in df[0:3].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index,row in df[3:6].iterrows():
+        st.header(row["title"])
 
