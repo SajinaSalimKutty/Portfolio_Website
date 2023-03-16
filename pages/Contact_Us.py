d0d3ae45f80526b0b324b97f5f98c853f56959d8
Sajina Salim Kutty <sajinassk03@gmail.com>
2023-03-16 15:52:14 +0530

    Mail sent using forms

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from send_mail import sent_mail
 
 st.header("Contact Us")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your Email Address")
+    raw_message = st.text_area("Your Message")
+    message = f"""\
+Subject:New Mail from {user_email}
+
+From:{user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    print(button)
+    if button:
+        sent_mail(message)
+        st.info("Your message was sent successfully")
