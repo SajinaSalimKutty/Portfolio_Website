d2babc799b3cc5f23a8da6cb3a3e1ce0b69c91ae
Sajina Salim Kutty <sajinassk03@gmail.com>
2023-03-16 15:53:04 +0530

    Mail sent

---
+++ send_mail.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+
+
+def sent_mail(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "sajinassk03@gmail.com"
+    password = "gqipprfzqgmzhtuz"
+    receiver = "sajinassk03@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
+
+
